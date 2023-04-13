26929b88a690c106df3b2db532d5ab5ae78280c0
SashaLifashkin <alifashkin@outlook.com>
2023-04-13 19:55:43 +0300

    global using Outlet

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,41 @@ import ReactDOM from 'react-dom/client';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
-import { App } from './App';
+import { App, TodosPage } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Navigate, Outlet, Routes, Route } from 'react-router-dom';
 
 const Root = () => (
     <BrowserRouter>
-        <App />
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route index element={<h1 className="title">Home Page</h1>} />
+                <Route path="home" element={<Navigate to="/" replace />} />
+
+                {/* Наследование роутеров */}
+                <Route path="todos">
+                    <Route index element={<TodosPage />} />
+                    <Route path=":todoId" element={<TodosPage />} />
+                </Route>
+
+                {/* Решение неоткрывающегося списка todos без наследования роутеров */}
+                {/* <Route
+                    path="todos"
+                    element={
+                        <TodosPage />
+                    }
+                />
+
+                <Route
+                    path="todos/:todoId"
+                    element={
+                        <TodosPage />
+                    }
+                /> */}
+
+                <Route path="*" element={<p>Page nof found</p>} />
+            </Route>
+        </Routes>
     </BrowserRouter>
 );
 
